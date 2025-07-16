ce82d7471e0841b80b91eb05f265d5950fb48e9e
CHAKRI <burrachakrapani123@gmail.com>
2025-07-16 20:50:08 +0530

    updated the swarm supervisor node and some changes in recharge node

--- gcs_ws/build/gcs_controller/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/bhav/IITISoC-25-IVR09/gcs_ws/install/drone_interfaces/share/drone_interfaces/package.sh"
-. "/home/bhav/IITISoC-25-IVR09/gcs_ws/install/mission_utils/share/mission_utils/package.sh"
