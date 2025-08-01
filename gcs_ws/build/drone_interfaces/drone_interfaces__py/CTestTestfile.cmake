51226722fd297dad05f5cc7e46a155c235d55839
CHAKRI <burrachakrapani123@gmail.com>
2025-08-01 15:48:02 +0530

    cleaning repo

--- gcs_ws/build/drone_interfaces/drone_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bhav/IITISoC-25-IVR09/gcs_ws/build/drone_interfaces/drone_interfaces__py
-# Build directory: /home/bhav/IITISoC-25-IVR09/gcs_ws/build/drone_interfaces/drone_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
